ba71dd6eeb4e74b751c1797ff3a1516538b24906
zapteryx <max@zptx.icu>
2019-08-01 19:52:38 +0800

    so dumb that i forgot people can leave servers xdxdxd

--- modules/MSU.js
+++ modules/MSU.js
@@ -3,7 +3,7 @@ var settings = require("../bot.js").settings;
 var data = require("../bot.js").data;
 
 module.exports.commands = [];
-module.exports.events = ["guildMemberAdd"];
+module.exports.events = ["guildMemberAdd", "guildMemberRemove"];
 module.exports.actions = function (type, cmd, body, obj) {
   if (type == "guildMemberAdd" && obj[0].id == "574949938929074186") {
     // Welcome Message
@@ -15,6 +15,10 @@ module.exports.actions = function (type, cmd, body, obj) {
         description: "Hey! Welcome to " + obj[1].guild.name + "!\n\n📌 Information:\n\n▸ Upgrade | <#575823400115896321>\n▸ Redeem | <#575823572203864064>\n▸ Support | <#575805177416646698>\n▸ Invite | https://discord.gg/enTsYhx"
       }
     });
+    bot.editChannel("590737065566666752", {name: "Member Count: " + obj[0].members.size});
+    bot.editChannel("589907400459419688", {name: "User Count: " + obj[0].members.filter(function(member){if (!member.bot) {return true;} else {return false;}}).length});
+  }
+  else if (type == "guildMemberRemove" && obj[0].id == "574949938929074186") {
     bot.editChannel("590737065566666752", {name: "Member Count: " + obj[0].members.size});
     bot.editChannel("589907400459419688", {name: "User Count: " + obj[0].members.filter(function(member){if (!member.bot) {return true;} else {return false;}}).length});
   }
